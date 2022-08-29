9331e214418726f6f37b67df5009be6c75d8a00b
uvadhar11 <111589719+uvadhar11@users.noreply.github.com>
2022-08-29 16:22:01 +0000

    Added new html file for the home page and submit form btn on log in page links you to the home page.

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-// signUpButton = document.querySelector(".sign-up-btn");
-// logInButton = document.querySelector(".log-in-btn");
-// volunteerLogo = document.querySelector(".logo");
-// featuresContentDiv = document.querySelector(".features-content");
+const logInBtn = document.querySelector(".log-in-form-submit-btn");
 
-// signUpButton.addEventListener("click", function () {
-//   console.log("Sign up button clicked!");
-//   featuresContentDiv.classList.add("hidden"); // this code adds the hidden class to the classList of the feature-content div
-// });
-
-// logInButton.addEventListener("click", function () {
-//   console.log("Log in button clicked!");
-// });
-
-// volunteerLogo.addEventListener("click", function () {
-//   console.log("Logo clicked!");
-// });
+logInBtn.addEventListener("click", function(event) {
+    console.log('a');
+    window.location="../home/index.html";
+});
