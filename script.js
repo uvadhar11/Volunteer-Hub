72be1745f1897e2f58281d01413256feb1037276
uvadhar11 <111589719+uvadhar11@users.noreply.github.com>
2022-08-30 16:52:20 +0000

    added react.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const logInBtn = document.querySelector(".log-in-form-submit-btn");
 
 logInBtn.addEventListener("click", function(event) {
     console.log('a');
-    window.location="../home/index.html";
+    window.location="../home";
 });
