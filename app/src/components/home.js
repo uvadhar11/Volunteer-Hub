f841043239336429112afa8164c0e61285e2bbac
uvadhar11 <111589719+uvadhar11@users.noreply.github.com>
2022-09-11 10:42:00 -0700

    added some icons and chakra ui

--- app/src/components/home.js
+++ app/src/components/home.js
@@ -1,11 +1,24 @@
+import { Box, Flex, IconButton } from "@chakra-ui/react";
+import React from "react";
+import {
+  FaBars,
+  FaSun,
+  FaMoon,
+  FaBell,
+  FaQuestionCircle,
+} from "react-icons/fa";
+
 function Home() {
   return (
-    <div>
-      <footer className="footer">
-        {/* <!-- Maybe put some stuff in here --> */}
-        <h1>Footer</h1>
-      </footer>
-    </div>
+    <Box bg="blackAlpha.800">
+      <Flex>
+        <IconButton aria-label="Menu" icon={<FaBars />}></IconButton>
+        <IconButton aria-label="Menu" icon={<FaSun />}></IconButton>
+        <IconButton aria-label="Menu" icon={<FaBell />}></IconButton>
+        <IconButton aria-label="Menu" icon={<FaQuestionCircle />}></IconButton>
+        <IconButton aria-label="Menu" icon={<FaMoon />}></IconButton>
+      </Flex>
+    </Box>
   );
 }
 
