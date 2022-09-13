a9c2c5988f7bc5fb16a72d84e115ac88096d0560
uvadhar11 <111589719+uvadhar11@users.noreply.github.com>
2022-09-13 09:32:28 -0700

    added basic sidebar panels

--- app/src/components/home.js
+++ app/src/components/home.js
@@ -1,4 +1,11 @@
-import { Box, Flex, IconButton, VStack } from "@chakra-ui/react";
+import {
+  Flex,
+  IconButton,
+  VStack,
+  useColorMode,
+  Spacer,
+  HStack,
+} from "@chakra-ui/react";
 import React from "react";
 import {
   FaBars,
@@ -7,12 +14,18 @@ import {
   FaBell,
   FaQuestionCircle,
   FaGrinAlt,
+  FaHome,
+  FaGrinTears,
 } from "react-icons/fa";
 import Logo from "./logo";
 
 function Home() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const isDark = colorMode === "dark"; // the value is this boolean expression so the value technically doesn;t change but the evaluation
+  // true/false doesn't change.
   return (
-    <VStack>
+    <VStack w="100%" h="100vh">
+      {/* nav bar */}
       <Flex w="100%" h="3em" bg="blackAlpha.600" pl="2" align="center">
         <IconButton
           aria-label="Menu Button"
@@ -20,10 +33,10 @@ function Home() {
           icon={<FaBars />}
         ></IconButton>
         <Logo />
+        <Spacer />
         <IconButton
           aria-label="Help Button"
           colorScheme="facebook"
-          ml="1000"
           icon={<FaQuestionCircle />}
         ></IconButton>
         <IconButton
@@ -32,21 +45,59 @@ function Home() {
           ml="2"
           icon={<FaBell />}
         ></IconButton>
-
         <IconButton
           aria-label="Theme Change"
           colorScheme="facebook"
           ml="2"
-          icon={<FaMoon />}
+          icon={isDark ? <FaSun /> : <FaMoon />}
+          onClick={toggleColorMode}
         ></IconButton>
-
         <IconButton
           aria-label="Profile"
           colorScheme="facebook"
           ml="2"
+          mr="2"
           icon={<FaGrinAlt />}
         ></IconButton>
       </Flex>
+
+      {/* side bar */}
+
+      {/* side bar */}
+      <HStack
+        width="100%"
+        height="100vh"
+        justify="flex-start"
+        align="flex-start"
+      >
+        <VStack
+          w="4%"
+          bg="blue"
+          h="100%"
+          justify="flex-start"
+          align="center"
+          alignSelf={"flex-start"}
+          gap="1"
+        >
+          <IconButton
+            mt="2"
+            aria-label="Home Button"
+            colorScheme="facebook"
+            icon={<FaHome />}
+          ></IconButton>
+
+          <IconButton
+            aria-label="Volunteer Opportunity"
+            colorScheme="facebook"
+            icon={<FaGrinTears />}
+          />
+        </VStack>
+
+        {/* side side bar (like channel bar) */}
+        <VStack bg="red" width="10em" height="100%">
+          <text>Channels</text>
+        </VStack>
+      </HStack>
     </VStack>
   );
 }
