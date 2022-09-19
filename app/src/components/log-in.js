ee101bddc0c16da583422627feb805becc3f2874
uvadhar11 <111589719+uvadhar11@users.noreply.github.com>
2022-09-18 17:20:36 -0700

    added basic sign in with google button (that does nothing yet, placeholder).

--- app/src/components/log-in.js
+++ app/src/components/log-in.js
@@ -1,4 +1,12 @@
-import { VStack, Text, Flex, Input, Center, Button } from "@chakra-ui/react";
+import {
+  VStack,
+  Text,
+  Flex,
+  Input,
+  Center,
+  Button,
+  HStack,
+} from "@chakra-ui/react";
 import React from "react";
 import NavBar from "./navbar";
 import { useNavigate } from "react-router-dom";
@@ -7,103 +15,66 @@ function LogIn() {
   let navigate = useNavigate();
 
   return (
-    // <div>
-    //   <nav className="top-bar">
-    //     <div>
-    //       <img src="app\src\logo.png" alt="logo" className="logo" />
-    //       <h3 className="volunteer-hub-logo-text">Volunteer Hub</h3>
-    //     </div>
-    //     <div className="LI-sign-up-btn">
-    //       <a href="/sign-up" className="sign-up-btn">
-    //         Sign Up
-    //       </a>
-    //     </div>
-    //   </nav>
-
-    //   {/* <!-- Log in to an existing account -->
-    //         <!-- Log in label (center the div, white background, background image maybe like a space one)-->
-    //         <!-- email or username (firstName + LastName)-->
-    //         <!-- password input field --> */}
-
-    //   <form className="log-in-form">
-    //     <h1>Log In</h1>
-
-    //     <label>Email</label>
-    //     <input required />
-
-    //     <label>Password</label>
-    //     <input type="password" required />
-
-    //     <input
-    //       type="button"
-    //       value="Log In"
-    //       className="log-in-form-submit-btn"
-    //     />
-    //   </form>
-
-    //   <button className="log-in-forgot-password-btn">Forgot Password?</button>
-
-    //   <footer className="footer">
-    //     {/* <!-- Maybe put some stuff in here --> */}
-    //     <h1>Footer</h1>
-    //   </footer>
-    // </div>
     <VStack>
       <NavBar />
       <Center w="100%" h="30em">
-        {/* Log in text */}
-        <Flex
-          w="50%"
-          h="21em"
-          marginTop={"20"}
-          bg="whiteAlpha.200"
-          justify="left"
-          align="left"
-          flexDirection="column"
-        >
-          <Text ml="3" mt="1.5" fontSize="3xl">
-            Log-In
-          </Text>
+        <HStack w="45%" h="21em" bg="whiteAlpha.200">
+          {/* Log in text */}
+          <Flex justify="left" align="left" flexDirection="column">
+            <Text ml="3" mt="1.5" fontSize="3xl">
+              Log-In
+            </Text>
 
-          {/* Email */}
-          <Text ml="3" mt="4" fontSize="xl">
-            Email
-          </Text>
-          <Input placeholder="Email" size="md" w="20em" mt="2" ml="3"></Input>
+            {/* Email */}
+            <Text ml="3" mt="4" fontSize="xl">
+              Email
+            </Text>
+            <Input placeholder="Email" size="md" w="20em" mt="2" ml="3"></Input>
 
-          {/* Password */}
-          <Text ml="3" mt="4" fontSize="xl">
-            Password
-          </Text>
-          <Input
-            placeholder="Password"
-            size="md"
-            w="20em"
-            mt="2"
-            ml="3"
-          ></Input>
-          <Button
-            size="xs"
-            w="-webkit-fit-content"
-            variant="link"
-            mt="1"
-            ml="3"
-            onClick={() => navigate("/forgot-password")}
-          >
-            Forgot Password?
-          </Button>
+            {/* Password */}
+            <Text ml="3" mt="4" fontSize="xl">
+              Password
+            </Text>
+            <Input
+              placeholder="Password"
+              size="md"
+              w="20em"
+              mt="2"
+              ml="3"
+            ></Input>
+            <Button
+              size="xs"
+              w="-webkit-fit-content"
+              variant="link"
+              mt="1"
+              ml="3"
+              onClick={() => navigate("/forgot-password")}
+            >
+              Forgot Password?
+            </Button>
 
-          {/* Log in button */}
-          <Button
-            mt="4"
-            ml="3"
-            size="md"
-            colorScheme="facebook"
-            w="-webkit-fit-content"
-          >
-            Log in
-          </Button>
-        </Flex>
+            {/* Log in button */}
+            <Button
+              mt="4"
+              ml="3"
+              size="md"
+              colorScheme="facebook"
+              w="-webkit-fit-content"
+            >
+              Log in
+            </Button>
+          </Flex>
+
+          {/* sign in with google */}
+          <Flex>
+            <Text fontSize="2xl" ml="10">
+              OR
+            </Text>
+            <Button size="md" ml="10" w="-webkit-fit-content">
+              Sign in with google
+            </Button>
+          </Flex>
+        </HStack>
       </Center>
     </VStack>
   );
