7f6c5765039b7423529982ebd21f2ba9d7a51e02
uvadhar11 <111589719+uvadhar11@users.noreply.github.com>
2022-10-25 09:54:13 -0700

    attempting to work on account settings

--- app/src/components/account-settings.js
+++ app/src/components/account-settings.js
@@ -4,24 +4,38 @@ import NavBar from "./navbar";
 
 function AccountSettings() {
   return (
-    <VStack>
+    <VStack w="100%" h="100vh">
       <NavBar />
       <Text fontSize="6xl">Account Settings</Text>
-      <HStack width="100%">
+      <HStack w="90%" pt="1%">
         {/* Buttons */}
-        <HStack
-          flexDirection="column"
-          align="left"
-          w="15vh"
+        <VStack w="17%" align="flex-start" justify="top">
+          <Button>Hello</Button>
+          <Button>Hello again</Button>
+        </VStack>
+
+        {/* Content */}
+        <VStack
+          w="90%"
+          maxW="90%"
+          align="flex-start"
           justify="center"
-          bg="blue"
+          pl="5%"
+          overflowWrap={"break-word"}
         >
-          <Button w="8vh">Hello</Button>
-          <Button w="8vh">Hello1</Button>
-          {/* <Button>Hello again</Button> */}
-        </HStack>
-        {/*  */}
-        <HStack></HStack>
+          {/*padding -> padding the content in a div*/}
+          <Text>Content</Text>
+          <Text w="100%">
+            Hello this is a filler content that we are going to be using to
+            write an essay. Lorem Ipsum Fill in balnk stuff. GAMMAMMAMMAJFojdfp
+            afpapsdifpasdifasdfpisadfpoasdufoasjdfo
+            jasdfjasasjdfasjdfajsd;lfjadfl;asjdflasjdf
+            adpfweij-viwqenprv[weutpvbuwq[jvuwrtveyrwv]] SDKf flexDirection
+            flexDirectiona;sljdf;lsajdfl;ajsdlf;jal;sdjfl;asdjfl;ajsdfl;asjdfljasdl;fjasl;dfjal;sdjfl;asjdfl;asjdfl;sjadfl;asjdfl;jsad;lfjasl;dfjlas;djfls;adjfl;asjdf;lsajdfl;asjdfl;jasdfl;jsad;lfjasl;dfja;ljfwaoejmrf0wier0wivn04tuuownuejldfjsladjfl;asjdfl;jasfl;sjaldf;jas;Hello
+            guys
+            jfqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
+          </Text>
+        </VStack>
       </HStack>
     </VStack>
   );
--- app/src/components/navbar.js
+++ app/src/components/navbar.js
@@ -151,6 +151,7 @@ function NavBar(props) {
           as={Avatar}
           aria-label="Profile"
           name={user ? user.displayName : null} // maybe use the user data in firestore instead of display name in auth.
+          pl="3" // padding so the profile picture is centered.
           ml="2"
           mr="2"
           colorScheme="facebook"
